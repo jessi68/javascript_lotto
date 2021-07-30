74cc3f0fe91fbe516ad77bd9c04ddb950858a201
jessi68 <lynn312.unist.ac.kr>
2021-07-30 17:20:55 +0900

    feat: 로또 수동 구매 입력 view mark up

---
+++ src/js/view/LottoManualInputView.js
@@ -0,0 +1,28 @@
+const lottoManualInputView = (number) => {
+    return `
+    <input
+    type="number"
+    class="manual-purchase-number mx-1 text-center"
+    />
+    <input
+    type="number"
+    class="manual-purchase-number mx-1 text-center"
+    />
+    <input
+    type="number"
+    class="manual-purchase-number mx-1 text-center"
+    />
+    <input
+    type="number"
+    class="manual-purchase-number mx-1 text-center"
+    />
+    <input
+    type="number"
+    class="manual-purchase-number mx-1 text-center"
+    />
+    <input
+    type="number"
+    class="manual-purchase-number mx-1 text-center"
+    />
+`;
+}
